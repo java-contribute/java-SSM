ba359a3effda3ae86d7e9b0a7b2f7ff790541413
Emilia-Yukino <lj-emilia@qq.com>
2018-06-22 16:29:23 +0800

    修改springMVC，只返回json数据

--- src/main/java/web/UserController.java
+++ src/main/java/web/UserController.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import service.UserLoginService;
+
+import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -23,8 +26,10 @@ public class UserController {
     private Logger LOGGER = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserLoginService userLoginService;
-    @RequestMapping(value = "/list" ,method = RequestMethod.GET)
-    private String UserList(){
+    @RequestMapping(value = "/list" ,method = RequestMethod.GET,produces = {
+            "application/json; charset=utf-8" })
+    @ResponseBody
+    private String UserList(HttpServletResponse response){
         Result result = new Result();
         result.setData(userLoginService.userQueryAll());
         return JSON.toJSONString(result,SerializerFeature.WriteMapNullValue);
--- src/test/java/test/web/UserWebTest.java
+++ src/test/java/test/web/UserWebTest.java
@@ -12,7 +12,6 @@ import test.BaseControllerTest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
