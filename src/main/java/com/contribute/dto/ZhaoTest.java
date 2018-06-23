47f2b58694a8a572e9723a5d955e3d09f8367f24
uiocool <1429583624@qq.com>
2018-06-23 22:30:44 +0800

    Github Test 111

--- src/main/java/com/contribute/dto/ZhaoTest.java
+++ src/main/java/com/contribute/dto/ZhaoTest.java
@@ -1,4 +1,13 @@
 package com.contribute.dto;
 
 public class ZhaoTest {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
